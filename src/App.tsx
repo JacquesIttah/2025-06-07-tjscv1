efe1d9bc6f6a6fdb330867ab16247fc4190483f3
champix56 <desorbaix@free.fr>
2025-07-08 16:32:26 +0200

    ajout d'un etat cetralisé pour le meme a éditer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import FlexVerticalThirdGrow from "./components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow";
 import Header from "./components/ui/Header/Header";
@@ -9,18 +9,22 @@ import Footer from "./components/ui/Footer/Footer";
 import { emptyMeme, MemeSVGViewer } from "orsys-tjs-meme";
 
 const App = () => {
+  const [meme,setMeme]=useState(emptyMeme)
   return (
-    <div className="App">
-      <FlexVerticalThirdGrow>
-        <Header></Header>
-        <Navbar></Navbar>
-        <FlexHorizontFirstGrow>
-          <MemeSVGViewer image={undefined} meme={emptyMeme} />
-          <MemeForm data="dk" />
-        </FlexHorizontFirstGrow>
-        <Footer/>
-      </FlexVerticalThirdGrow>
-    </div>
+    <>
+      <div>{JSON.stringify(emptyMeme)}</div>
+      <div className="App">
+        <FlexVerticalThirdGrow>
+          <Header></Header>
+          <Navbar></Navbar>
+          <FlexHorizontFirstGrow>
+            <MemeSVGViewer image={undefined} meme={meme} />
+            <MemeForm meme={meme} onMemeChange={(m)=>{setMeme(m)}} />
+          </FlexHorizontFirstGrow>
+          <Footer />
+        </FlexVerticalThirdGrow>
+      </div>
+    </>
   );
 };
 
