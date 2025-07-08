fd793ee2f5bf070b3fa62974e4de90108f46f133
champix56 <desorbaix@free.fr>
2025-07-08 10:12:08 +0200

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/ui/Button/Button.jsx";
 function App() {
-  let counter=0;
+  const [counter, setCounter] = useState(0)
   return (
     <div style={{textAlign:'center'}}>
       <div>Valeur de counter : {counter}</div>
       <hr />
       <Button bgColor="skyblue" type="button" onButtonClick={(arg)=>{
-        counter--;
+        setCounter(counter-1);
         console.log(counter);
       }} >
         -1
       </Button>
       <Button bgColor="aquamarine" type="reset" onButtonClick={(arg)=>{
-         counter++;
+         setCounter(counter+1)
          console.log(counter);
       }}>
         +1
