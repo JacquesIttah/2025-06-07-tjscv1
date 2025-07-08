200bc28ce45e39746ac7aaa4e47729f341500444
champix56 <desorbaix@free.fr>
2025-07-08 14:22:31 +0200

    interface du layout 1

--- src/components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow.tsx
+++ src/components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow.tsx
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./FlexVerticalThirdGrow.module.css";
 
 interface IFlexVerticalThirdGrowProps {
-  data:string
+  children: Array<React.ReactElement> | React.ReactElement;
+  style?: object;
 }
-interface IFlexVerticalThirdGrowState {
-  value:number
-}
-const flexVerticalThirdGrowInitialState = {value:0};
-const FlexVerticalThirdGrow: React.FC<IFlexVerticalThirdGrowProps> = ({data=""}) => {
-  const [state, setstate] = useState<IFlexVerticalThirdGrowState>(
-    flexVerticalThirdGrowInitialState
-  );
-  useEffect(() => {
-    return () => {
-      //demontage
-    };
-  }, []);
+
+const FlexVerticalThirdGrow: React.FC<IFlexVerticalThirdGrowProps> = ({
+  data = "",
+}) => {
   return (
-    <div className={styles.FlexVerticalThirdGrow} data-testid="FlexVerticalThirdGrow">
-      FlexVerticalThirdGrow Component props.data:{data}, state:{JSON.stringify(state)}
+    <div
+      className={styles.FlexVerticalThirdGrow}
+      data-testid="FlexVerticalThirdGrow"
+    >
+      FlexVerticalThirdGrow Component props.data:{data}, state:
+      {JSON.stringify(state)}
     </div>
   );
 };
