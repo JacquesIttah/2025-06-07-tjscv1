30242f8f792c8bc04e7a8024f04f66c2f8ce4683
champix56 <desorbaix@free.fr>
2025-07-08 20:36:28 +0200

    Links for router

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -4,17 +4,18 @@ import { Navbar as Nvb } from "react-bootstrap";
 import { Container, Nav } from "react-bootstrap";
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../../../../node_modules/bootstrap/dist/js/bootstrap.js";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC<object> = () => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
       <Nvb bg="primary" data-bs-theme="dark">
         <Container>
-          <Nvb.Brand href="#home">Home</Nvb.Brand>
+          <Nvb.Brand as={Link} to="/">Home</Nvb.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">New</Nav.Link>
-            <Nav.Link href="#features">List</Nav.Link>
-            <Nav.Link href="#pricing">edit/1</Nav.Link>
+            <Nav.Link as={Link} to={'/editor'}>New</Nav.Link>
+            <Nav.Link as={Link} to={'/editor/0'}>edito 0</Nav.Link>
+            <Nav.Link as={Link} to={'/thumbnail'}>list</Nav.Link>
           </Nav>
         </Container>
       </Nvb>
