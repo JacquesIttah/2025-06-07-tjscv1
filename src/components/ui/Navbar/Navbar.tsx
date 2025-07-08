ad11e3cd39fb5ad453195cd9484b3ba1988060b4
champix56 <desorbaix@free.fr>
2025-07-08 18:16:40 +0200

    add react-bootstrap bootstrap

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { Navbar as Nvb } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../../../node_modules/@popperjs/core/dist/cjs/popper.js";
+import "../../../../node_modules/bootstrap/dist/js/bootstrap.js";
 
 const Navbar: React.FC<object> = () => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
-      Navbar Component
+      <Nvb bg="primary" data-bs-theme="dark">
+        <Container>
+          <Nvb.Brand href="#home">Home</Nvb.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">New</Nav.Link>
+            <Nav.Link href="#features">List</Nav.Link>
+            <Nav.Link href="#pricing">edit/1</Nav.Link>
+          </Nav>
+        </Container>
+      </Nvb>
     </div>
   );
 };
