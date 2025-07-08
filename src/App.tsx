54333badfaf3825321e71e5e8ebec1950a2b18a9
champix56 <desorbaix@free.fr>
2025-07-08 10:17:50 +0200

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/ui/Button/Button.jsx";
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    //obeservation de changements async d'etats
+    console.log(counter,'modified');
+  }, [counter])
   return (
     <div style={{textAlign:'center'}}>
       <div>Valeur de counter : {counter}</div>
       <hr />
       <Button bgColor="skyblue" type="button" onButtonClick={(arg)=>{
         setCounter(counter-1);
+        //non synchrone avec le changement de valeur
         console.log(counter);
       }} >
         -1
       </Button>
       <Button bgColor="aquamarine" type="reset" onButtonClick={(arg)=>{
          setCounter(counter+1)
+        //non synchrone avec le changement de valeur
          console.log(counter);
       }}>
         +1
