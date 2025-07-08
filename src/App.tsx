3b9112a0a9944101f26224d3b7b077f676d920d4
champix56 <desorbaix@free.fr>
2025-07-08 14:44:57 +0200

    structure de mon App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import Button from "./components/ui/Button/Button.jsx";
-import MemeForm from "./components/MemeForm/MemeForm.js";
-function App() {
-  const [counter, setCounter] = useState(0);
-  useEffect(() => {
-    //obeservation de changements async d'etats
-    console.log(counter,'modified');
-  }, [counter])
+import FlexVerticalThirdGrow from "./components/layout/FlexVerticalThirdGrow/FlexVerticalThirdGrow";
+import Header from "./components/ui/Header/Header";
+import Navbar from "./components/ui/Navbar/Navbar";
+import FlexHorizontFirstGrow from "./components/layout/FlexHorizontFirstGrow/FlexHorizontFirstGrow";
+import MemeSvgVierwer from "./components/ui/MemeSVGVierwer/MemeSvgVierwer";
+import MemeForm from "./components/MemeForm/MemeForm";
+
+const App = () => {
   return (
-    <div style={{textAlign:'center'}}>
-      <MemeForm/>
-      <div>Valeur de counter : {counter}</div>
-      <hr />
-      <Button bgColor="skyblue" type="button" onButtonClick={(arg)=>{
-        setCounter(counter-1);
-        //non synchrone avec le changement de valeur
-        console.log(counter,arg);
-      }} >
-        -1
-      </Button>
-      <Button bgColor="aquamarine" type="reset" onButtonClick={(arg)=>{
-         setCounter(counter+1)
-        //non synchrone avec le changement de valeur
-         console.log(counter);
-      }}>
-        +1
-      </Button>
+    <div className="App">
+      <FlexVerticalThirdGrow>
+        <Header></Header>
+        <Navbar></Navbar>
+        <FlexHorizontFirstGrow>
+          <MemeSvgVierwer />
+          <MemeForm />
+        </FlexHorizontFirstGrow>
+      </FlexVerticalThirdGrow>
     </div>
   );
-}
+};
 
 export default App;
