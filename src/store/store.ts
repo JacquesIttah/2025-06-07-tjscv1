b6d132f5a5c16142e4a7d8d1d6b2f4a2e4d151b0
champix56 <desorbaix@free.fr>
2025-07-09 12:23:15 +0200

    ajout des typages auto en export

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,3 +5,7 @@ export const store = configureStore({
   reducer: combineReducers({ current: currentReducer }),
 });
 store.subscribe(()=>console.log(store.getState()))
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
