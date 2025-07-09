9d79e211fd1b0c7789805172bdbf45ec9ed5a650
champix56 <desorbaix@free.fr>
2025-07-09 17:57:39 +0200

    format

--- src/pages/Thumbnail.tsx
+++ src/pages/Thumbnail.tsx
@@ -7,11 +7,31 @@ function Thumbnail() {
   const memes = useSelector((s: RootState) => s.ressources.memes);
   const images = useSelector((s: RootState) => s.ressources.images);
   return (
-    <div className="Thumbnail" style={{display:'flex',gap:'20px',padding:'20px', overflowY:'scroll', flexWrap:'wrap', justifyContent:'space-between'}}>
+    <div
+      className="Thumbnail"
+      style={{
+        display: "flex",
+        gap: "20px",
+        padding: "20px",
+        overflowY: "scroll",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+      }}
+    >
       {memes.map((m, position) => {
-        
         return (
-          <Link style={{width:'40%',textAlign:'center',padding:'15px',alignSelf:'center', border:'1px solid grey', borderRadius:'8px' }} to={"/editor/" + m.id} key={"l" + position}>
+          <Link
+            style={{
+              width: "40%",
+              textAlign: "center",
+              padding: "15px",
+              alignSelf: "center",
+              border: "1px solid grey",
+              borderRadius: "8px",
+            }}
+            to={"/editor/" + m.id}
+            key={"l" + position}
+          >
             {m.titre}
             <MemeSVGViewer
               image={images.find((i) => i.id === m.imageId)}
