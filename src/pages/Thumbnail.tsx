75c7954bba751374b35f21879b9afefa01aa7b8e
champix56 <desorbaix@free.fr>
2025-07-09 17:57:27 +0200

    correction

--- src/pages/Thumbnail.tsx
+++ src/pages/Thumbnail.tsx
@@ -1,17 +1,25 @@
 import { MemeSVGViewer } from "orsys-tjs-meme";
+import { useSelector } from "react-redux";
 import { Link } from "react-router";
+import type { RootState } from "../store/store";
 
 function Thumbnail() {
+  const memes = useSelector((s: RootState) => s.ressources.memes);
+  const images = useSelector((s: RootState) => s.ressources.images);
   return (
-    <div className="Thumbnail">
+    <div className="Thumbnail" style={{display:'flex',gap:'20px',padding:'20px', overflowY:'scroll', flexWrap:'wrap', justifyContent:'space-between'}}>
       {memes.map((m, position) => {
-        <Link to={"/editor/" + m.id} key={"l" + position}>
-          <MemeSVGViewer
-            image={images.find((i) => i.id === m.imageId)}
-            meme={m}
-            basePath=""
-          />
-        </Link>;
+        
+        return (
+          <Link style={{width:'40%',textAlign:'center',padding:'15px',alignSelf:'center', border:'1px solid grey', borderRadius:'8px' }} to={"/editor/" + m.id} key={"l" + position}>
+            {m.titre}
+            <MemeSVGViewer
+              image={images.find((i) => i.id === m.imageId)}
+              meme={m}
+              basePath=""
+            />
+          </Link>
+        );
       })}
     </div>
   );
